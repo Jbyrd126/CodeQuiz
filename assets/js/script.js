1e37efd36bcafd854d527bc107e9180541d33701
Rob <jbyrd126@gmail.com>
2024-01-03 19:29:18 -0600

    linked in style sheets. Made conatiner and rows for highscore link and timer.

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Questions
+const questions = [
+    {
+        question: "How do we create an object?",
+        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
+        correct: "Curly Braces"
+    },
+
+    {
+        question: "How do we create an object?",
+        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
+        correct: "Curly Braces"
+    },
+    {
+        question: "How do we create an object?",
+        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
+        correct: "Curly Braces"
+    },
+    {
+        question: "How do we create an object?",
+        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
+        correct: "Curly Braces"
+    },
+]
