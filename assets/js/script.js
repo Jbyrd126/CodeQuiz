30a633a73fd1b0e47a8d1206da9f8ce627e2acc6
Rob <jbyrd126@gmail.com>
2024-01-03 23:53:49 -0600

    Got the timer to start on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
-// Questions
-const questions = [
-    {
-        question: "How do we create an object?",
-        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
-        correct: "Curly Braces"
-    },
+// Selects element by class
+var timeEl = document.querySelector("#clock");
 
-    {
-        question: "How do we create an object?",
-        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
-        correct: "Curly Braces"
-    },
-    {
-        question: "How do we create an object?",
-        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
-        correct: "Curly Braces"
-    },
-    {
-        question: "How do we create an object?",
-        answers: ["Square Brackets", "Double Quotes", "Curly Braces"],
-        correct: "Curly Braces"
-    },
-]
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 60;
+
+function countdown() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            sendMessage();
+        }
+
+    }, 1000);
+}
+
+function start() {
+    countdown();
+
+}
+
+function lose() {
+
+}
